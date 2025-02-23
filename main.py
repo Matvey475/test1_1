4b532fe7d1e22de7152cdcc666a796a85f83c7d7
Matvey475 <103kuri228668@mail.ru>
2025-02-23 21:25:34 +0700

    Create main.py
    test

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+# Удиви своих одногруппников!
+#my_words = ["Hi!", "Python Pro", "Kodland"] 
+#print(my_words[1])
+
+#name = input("Ваше имя?")
+#print("Hi ", name)
+
+
+#emojis = ["^^", "0_o", ":)", "¯\_(ツ)_/¯", "(￢_￢)"]
+#print(random.choice(emojis))
+
+list = ["Занимаюсь програмированием", "Играю в волебол", "Играю в баскетбол", "Ранее изучал HTML", "Мне 14 лет"]
+print('Случайный факт обо мне:')
+#for i in range(10):
+print(random.choice(list))
